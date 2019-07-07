8b27367a7aefdddea6d99a2400e51010eae5f5cb
saba4good <33079861+saba4good@users.noreply.github.com>
2019-07-07 12:27:21 +0900

    Update extractPolicies.py
    Not yet tested

--- extractPolicies.py
+++ extractPolicies.py
@@ -5,9 +5,7 @@ B format: Juniper FW shell 에서 'show security policies detail' 명령어의 o
 (putty 에서 읽을 수 있는 로그 파일 저장하면 됨.)
 방법: 해당 파이선 프로그램과 A, B 파일을 모두 한 폴더 안에 넣어두고, 그 폴더에서 다음 명령으로 실행시킨다.
      python ip-finder-v.1.0.4.py [ip list file A] [show security policies detail output file B]
-
 A 파일을 'ips'라는 list로 저장해두고, B 파일을 한 라인씩 읽으면서 ips를 검색해서 있으면 결과 파일에 저장한다.
-
 '''
 #!/usr/bin/env python3
 import mmap
@@ -22,26 +20,24 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('indiv_ip_file', type=argparse.FileType('r'))
     parser.add_argument('policy_file', type=argparse.FileType('r'))
-    
+
     args = parser.parse_args()
-    args.policy_file
-    args.indiv_ip_file
+    #args.policy_file
+    #args.indiv_ip_file
 
     ips = [ip.rstrip('\n') for ip in args.indiv_ip_file]  # ip가 있는 파일에서 ip 를 list로 추출
-    # candidates = []
     with args.policy_file as p_file, \
          open(OUTPUT_FILE, 'w') as r_file:
         for line in p_file:
-            if (line.find(THIS_IS_POL) != -1): #if the line is a start of a policy
-                the_policy = re.search(r'[:\s][_\-\w]+,',line) # Policy: 뒤에 나올 수 있는 정책 이름 추출 (delimiter= ':' or ' ' and ',')
-            else:
+            if THIS_IS_POL in line: #if the line is a start of a policy
+                thePolicy = (re.search(r':\s[_\-\w]+,',line)).strip(': ').rstrip(',')  # Policy: 뒤에 나올 수 있는 정책 이름 추출 (delimiter= ':' or ' ' and ',')
+            elif re.search(r'[\d]+\.[\d]+\.[\d]+/[\d]+', line):  ## IPv4 정보가 있는 line인지 확인한다
                 for ip in ips:
-                    if (line.find(ip) != -1):
+                    if ip in line:
                         # candidates.append(the_policy)
-                        if the_policy:
-                            r_file.write("%s, %s\n" % (the_policy.group()[1:len(the_policy.group())-1], ip))
+                        if thePolicy:
+                            r_file.write("%s, %s\n" % (thePolicy, ip))
                             # https://stackoverflow.com/questions/47078585/python-f-write-is-not-taking-more-arguments
                             # print(the_policy.group()[1:len(the_policy.group())-1], ' , ', ip)
                         else:
                             print('Error: Policy name not found')
-
