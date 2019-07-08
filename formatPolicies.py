7b858ae5478c15201e637bf5096896c8c9aecd11
saba4good <33079861+saba4good@users.noreply.github.com>
2019-07-08 15:57:45 +0900

    v.0.0.1
    - read in 2 input files from the arguments given in command line
    - create a dictionary with policy names and ip list from an input file
    - write an output file with the policies in the dictionary, extracting the policy bodies from the input log file
    - using the IP list in the dictionary is not implemented

--- formatPolicies.py
+++ formatPolicies.py
@@ -4,7 +4,7 @@ A format: policy name, ip pair list line by line
 B format: Juniper FW shell 에서 'show security policies detail | no-more' 명령어의 output을 텍스트로 저장한 파일
 (putty 에서 읽을 수 있는 로그 파일 저장하면 됨.)
 방법: 해당 파이선 프로그램과 A, B 파일을 모두 한 폴더 안에 넣어두고, 그 폴더에서 다음 명령으로 실행시킨다.
-     python formatPolicies.v.1.0.1.py [policy-ip list file A] [show security policies detail output file B]
+     python formatPolicies.v.1.0.1.py [policy-ip pair list file A] [show security policies detail output file B]
 - A 파일을 {policy name:[ip list]} dictionary 로 저장한다.
 - B 파일을 한 라인씩 읽으면서 {policy name:[ip list]}를 검색해서 policy name이 있으면 policy의 source, destination, port 순으로 읽어서 list를 만든다.
 - source, destination 리스트 중 dictionary 의 [ip list]와 중복되는 ip가 존재하면 [ip list]를 대신 파일에 저장한다.
@@ -39,6 +39,8 @@ Policy: a_name_for_policy, action-type: permit, State: enabled, Index: 65, Scope
 import mmap
 import argparse
 import re              # regular expression
+from datetime import date
+
 ### global variables
 THIS_IS_POL='Policy:'
 THIS_IS_ACTION='action-type:'
@@ -48,13 +50,13 @@ FOLLOWING_IS_SRC='Source addresses:'
 FOLLOWING_IS_DST='Destination addresses:'
 THIS_IS_PROTO='IP protocol:'
 THIS_IS_DPORT='Destination port range:'
-OUTPUT_FILE='policies-formatted-output.txt'  # 결과 파일 이름
+OUTPUT_FILE='output-policies-formatted-' + str(date.today()) + '.txt'  # 결과 파일 이름
 
 if __name__ == '__main__':
     # 이 프로그램을 실행할 때, 받아들일 arguments 2개
     parser = argparse.ArgumentParser()
-    parser.add_argument('pair_file', type=argparse.FileType('r'), required=True, help="a file with a list of policy name and ip pairs")
-    parser.add_argument('policy_file', type=argparse.FileType('r'), required=True, help="log for 'show security policies detail | no-more'")
+    parser.add_argument('pair_file', type=argparse.FileType('r'), help="a file with a list of policy name and ip pairs")
+    parser.add_argument('policy_file', type=argparse.FileType('r'), help="log for 'show security policies detail | no-more'")
 
     args = parser.parse_args()
 
@@ -62,9 +64,10 @@ if __name__ == '__main__':
     prevPol = ''
     with args.pair_file as pairs:
         for line in pairs:
-            thePolicy = (re.search(r'[_\-\w]+,',line)).rstrip(',') # 정책 이름 추출 (delimiter= before ',')
-            theIP     = (re.search(r',\s[_\-\w]+',line)).strip(', ') # IP 추출 (delimiter= after ',')
-            print("'",thePolicy, "' : '", theIP, "'")
+            # https://stackoverflow.com/questions/15340582/python-extract-pattern-matches
+            thePolicy = (re.search(r'[_\-\w]+,',line)).group(0).rstrip(',') # 정책 이름 추출 (delimiter= before ',')
+            theIP     = (re.search(r'(?<=,\s)[\d]+\.[\d]+\.[\d]+\.[\d]+',line)).group(0) # IP 추출 (delimiter= after ',') ## https://docs.python.org/3/library/re.html
+            print(thePolicy+": "+theIP+";")
             if thePolicy == prevPol:
                 policyIPsPair[thePolicy].append(theIP)
             else:
@@ -72,37 +75,39 @@ if __name__ == '__main__':
                 prevPol = thePolicy
 
     with args.policy_file as p_file, \
-         open(OUTPUT_FILE, 'w') as out_file:
-        skip = false
+        open(OUTPUT_FILE, 'w') as out_file:
+        skip = False
         k = 1
         for line in p_file:
             if THIS_IS_POL in line: #if the line is a start of a policy
-                policy = (re.search(r':\s[_\-\w]+,',line)).strip(': ').rstrip(',') # Policy: 뒤에 나올 수 있는 정책 이름 추출 (delimiter= ':' or ' ' and ',')
+                policy = (re.search(r'(?<=:\s)[_\-\w]+,',line)).group(0).rstrip(',') # Policy: 뒤에 나올 수 있는 정책 이름 추출 (delimiter= ':' or ' ' and ',')
                 if policy in policyIPsPair:
-                    out_file.write("%d\) " % (k))
+                    out_file.write("\n%d) " % (k))
                     k += 1
                     nPorts = 0
-                    skip = false
+                    skip = False
                 else:
-                    skip = true
+                    skip = True
             elif not skip:
                 if THIS_IS_FROM in line:
-                    out_file.write("%s->" % ((re.search(r':\s[_\-\w]+,',line)).strip(': ').rstrip(',')))
-                    regexTemp = re.escape(THIS_IS_TO) + r':\s[_\-\w]+'
-                    out_file.write("%s 구간" % ((re.search(regexTemp,line)).replace(THIS_IS_TO + ': ', '')))
+                    out_file.write("%s->" % ((re.search(r'(?<=:\s)[_\-\w]+,',line)).group(0).rstrip(',')))
+                    regexTemp = re.escape(THIS_IS_TO) + r'\s[_\-\w]+'
+                    out_file.write("%s 구간 %s\n" % ((re.search(regexTemp,line)).group(0).replace(THIS_IS_TO + ' ', ''), policy))
                 elif FOLLOWING_IS_SRC in line:
                     out_file.write("- SIP: \n")
                 elif FOLLOWING_IS_DST in line:
                     out_file.write("- DIP: \n")
-                elif re.search(r'[\d]+\.[\d]+\.[\d]+/[\d]+', line):   #### This only searches for IPv4 addresses
-                    out_file.write("%s\n" % re.search(r'[\d]+\.[\d]+\.[\d]+/[\d]+', line))
+                elif re.search(r'[\d]+\.[\d]+\.[\d]+\.[\d]+/[\d]+', line):   #### This only searches for IPv4 addresses
+                    ### https://stackoverflow.com/questions/1038824/how-do-i-remove-a-substring-from-the-end-of-a-string-in-python
+                    ipTemp = re.sub(r'/32$', '', re.search(r'[\d]+\.[\d]+\.[\d]+\.[\d]+/[\d]+', line).group(0))
+                    out_file.write("%s\n" % ipTemp)
                 elif THIS_IS_PROTO in line:
-                    if not n_ports:
+                    if not nPorts:
                         out_file.write("- PORT: \n")
-                    out_file.write("%s " % ((re.search(r':\s[_\-\w]+,',line)).strip(': ').rstrip(',')))
+                    out_file.write("%s " % ((re.search(r'(?<=:\s)[_\-\w]+,',line)).group(0).rstrip(',')))
                 elif THIS_IS_DPORT in line:
-                    start = int((re.search(r'\[[\d]+\-',line)).strip('[').rstrip('-'))
-                    end   = int((re.search(r'\-[\d]+\]',line)).strip('-').rstrip(']'))
+                    start = int((re.search(r'(?<=\[)[\d]+\-',line).group(0)).rstrip('-'))
+                    end   = int((re.search(r'(?<=\-)[\d]+\]',line).group(0)).rstrip(']'))
                     nThisSrv = end - start
                     if not nThisSrv:
                         out_file.write("%d\n" % (start))
