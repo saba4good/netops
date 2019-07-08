1513b3ecf20f87879261f357cd3ed3536c575bd2
saba4good <33079861+saba4good@users.noreply.github.com>
2019-07-08 14:27:01 +0900

    a few fixes
    - r'\.[\d]+\.[\d]+\.[\d]+/[\d]+' ==> r'[\d]+\.[\d]+\.[\d]+\.[\d]+/[\d]+'  : 처음 부분 빠진 것 보완
    - tab -> 4 spaces

--- extractPolicies.py
+++ extractPolicies.py
@@ -27,15 +27,15 @@ if __name__ == '__main__':
     
     ips = [ip.rstrip('\n') for ip in args.indiv_ip_file]  # ip가 있는 파일에서 ip 를 list로 추출
     with args.policy_file as p_file, \
-         open(OUTPUT_FILE, 'w') as r_file:
+        open(OUTPUT_FILE, 'w') as r_file:
         for line in p_file:
             if THIS_IS_POL in line: #if the line is a start of a policy
-			    ### 1) re.search() returns an object; so to get a string found, use returned_object.group(0)
-				### reference: https://stackoverflow.com/questions/15340582/python-extract-pattern-matches
-				### 2) '(?<=...)' is a positive lookbehind assertion. 이것을 사용하려면 ...에 해당하는 string이 fixed length여야 한다.
-				### reference: https://docs.python.org/3/library/re.html
+                ### 1) re.search() returns an object; so to get a string found, use returned_object.group(0)
+                ### reference: https://stackoverflow.com/questions/15340582/python-extract-pattern-matches
+                ### 2) '(?<=...)' is a positive lookbehind assertion. 이것을 사용하려면 ...에 해당하는 string이 fixed length여야 한다.
+                ### reference: https://docs.python.org/3/library/re.html
                 thePolicy = (re.search(r'(?<=:\s)[_\-\w]+,',line)).group(0).rstrip(',')  # Policy: 뒤에 나올 수 있는 정책 이름 추출 (delimiter= ':' or ' ' and ',')
-            elif re.search(r'[\d]+\.[\d]+\.[\d]+/[\d]+', line):  ## IPv4 정보가 있는 line인지 확인한다
+            elif re.search(r'[\d]+\.[\d]+\.[\d]+\.[\d]+/[\d]+', line):  ## IPv4 정보가 있는 line인지 확인한다
                 for ip in ips:
                     if ip in line:
                         # candidates.append(the_policy)
