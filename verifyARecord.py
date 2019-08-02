c01951e3c03949cec5b71ef1ef8ab31e5f4f179c
saba4good <33079861+saba4good@users.noreply.github.com>
2019-08-02 19:53:42 +0900

    Rollback
    rollback을 어떻게 하는지 모르겠음.

--- verifyARecord.py
+++ verifyARecord.py
@@ -24,20 +24,24 @@ Verification result:  {'fastbox.ezwel.com': True, 'm.fastbox.ezwel.com': True, '
 #!/usr/bin/env python3
 import argparse        # commandline arguments
 import re              # regular expression
+import dns.resolver    # to use this library, the package has to be installed  'sudo apt install python3-dnspython'
+#from datetime import date
 from subprocess import check_output  ### to use windows command https://stackoverflow.com/questions/14894993/running-windows-shell-commands-with-python
 ### Global variables
 THIS_IS_ROOT='도메인'  # domain paragraph가 시작하는 것을 알 수 있는 구문
+### https://stackoverflow.com/questions/32490629/getting-todays-date-in-yyyy-mm-dd-in-python
+#OUTPUT_FILE='output-nslookup-' + str(date.today()) + '.txt'  # 결과 파일 이름
 
 if __name__ == '__main__':
     # 이 프로그램을 실행할 때, 받아들일 arguments 2개
     parser = argparse.ArgumentParser()
-    parser.add_argument('dns_req_file', type=argparse.FileType('r', encoding='UTF-8'))  ### https://stackoverflow.com/questions/20994352/unicodeencodeerror-cp949-codec-cant-encode-character-u20a9-in-position-90
+    parser.add_argument('dns_req_file', type=argparse.FileType('r'))
     parser.add_argument('csr_id', type=str, nargs='?', default='000000')  ### nargs='?' make the argument optional with a default value
     args = parser.parse_args()
-    
+
     ### https://stackoverflow.com/questions/4033723/how-do-i-access-command-line-arguments-in-python
     outfile = 'CSR-' + args.csr_id + '-output-nslookup.txt'  # 결과 파일 이름
-    
+
     domains = []
     ipMappingList = []
     #with args.dns_req_file as requests, open(OUTPUT_FILE, 'w') as r_file:
@@ -53,26 +57,25 @@ if __name__ == '__main__':
             elif re.search(r'[\d]+\.[\d]+\.[\d]+\.[\d]+', line):  ## IPv4 정보가 있는 line인지 확인한다
                 domains.append(re.search(r'(?<=[:\s])[\.a-zA-Z]+',line).group(0) + "." + theRoot)
                 ipMappingList.append(re.search(r'[\d]+\.[\d]+\.[\d]+\.[\d]+', line).group(0))
-                
+
     print("domains:   ", domains)
     print("IPs:       ", ipMappingList)
-    
-    commands = ''
+
     ipMappedList = []
     with open(outfile, 'w') as r_file:
         for domain in domains:
-            commands = "nslookup " + domain
-            ### https://stackoverflow.com/questions/14894993/running-windows-shell-commands-with-python
-            ### cmd code page 확인하는 방법: D:\>chcp  https://docs.microsoft.com/en-us/windows-server/administration/windows-commands/chcp
-            ### https://thecoollife.tistory.com/18
-            ### codepage 949 https://en.wikipedia.org/wiki/Unified_Hangul_Code
-            lookupOutput = check_output(commands, shell=True).decode(encoding="CP949")
-            r_file.write("%s\n%s" % (commands, lookupOutput))
-            ### In multiline mode, ^ matches the position immediately following a newline and $ matches the position immediately preceding a newline.
-            ### https://stackoverflow.com/questions/587345/regular-expression-matching-a-multiline-block-of-text
-            ### https://stackoverflow.com/questions/33232729/how-to-search-for-the-last-occurrence-of-a-regular-expression-in-a-string-in-pyt
-            ### https://docs.python.org/3/library/re.html#re.Match.group
-            ipMappedList.append(re.search("(?s:.*)(?<=[:\s])([\d]+\.[\d]+\.[\d]+\.[\d]+)", lookupOutput).group(1))
+            ### https://stackoverflow.com/questions/13842116/how-do-we-get-txt-cname-and-soa-records-from-dnspython
+            ### https://stackoverflow.com/questions/3898363/set-specific-dns-server-using-dns-resolver-pythondns
+            print("domain: ", domain)
+            ### https://stackoverflow.com/questions/9245067/is-it-reasonable-in-python-to-check-for-a-specific-type-of-exception-using-isins
+            try:
+                answer = dns.resolver.query(domain, "A")
+                for data in answer:
+                    r_file.write("\nnslookup %s\n%s" % (domain, data.address))
+                    ipMappedList.append(data.address)
+            except dns.resolver.NXDOMAIN:
+                r_file.write("\nnslookup %s" % (domain))
+                r_file.write("\nNo such domain %s\n" % (domain))
     #print("IPs mapped: ", ipMappedList)
     verification = dict()
     for idx, ipMapped in enumerate(ipMappedList):
