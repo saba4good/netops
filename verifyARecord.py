4c8864d7e31809d03328f7b1e429af7b9f67884c
saba4good <33079861+saba4good@users.noreply.github.com>
2019-10-02 18:00:34 +0900

    Test if host names have the root domain in it
    To make it work even if the host names in the input file have the root domain name attached to them.

--- verifyARecord.py
+++ verifyARecord.py
@@ -3,36 +3,40 @@ This is to verify hosts and its IP address in a DNS A record
 Input file: domain name, host name, IP address
 예제:
  * 추가 할 호스트 , IP
-1)도메인 : ezwel.com
+1)도메인 : ez.com
 추가 할 호스트 , IP
-호스트 : fastbox IP :222.231.44.170
-호스트 : m.fastbox IP :222.231.44.170
-2)도메인 : weltree.com
+호스트 : fast IP :192.231.44.170
+호스트 : m.fast IP :192.231.44.170
+2)도메인 : tree.com
 추가 할 호스트 , IP
-호스트 : fastbox IP :222.231.44.210
+호스트 : fast IP :192.231.44.210
 사용방법: 해당 파이선 프로그램과 input 파일을 모두 한 폴더 안에 넣어두고, 그 폴더에서 다음 명령으로 실행시킨다.
      python verifyARecord.py [input file ] [CSR ID]
 예제:
 λ python verifyARecord.py test02.txt 617893
-domains:    ['fastbox.ezwel.com', 'm.fastbox.ezwel.com', 'fastbox.weltree.com']
-IPs:        ['222.231.44.170', '222.231.44.170', '222.231.44.210']
+domains:    ['fast.ez.com', 'm.fast.ez.com', 'fast.tree.com']
+IPs:        ['192.231.44.170', '192.231.44.170', '192.231.44.210']
 권한 없는 응답:
 권한 없는 응답:
 권한 없는 응답:
-Verification result:  {'fastbox.ezwel.com': True, 'm.fastbox.ezwel.com': True, 'fastbox.weltree.com': True}
+Verification result:  {'fast.ez.com': True, 'm.fast.ez.com': True, 'fast.tree.com': True}
 '''
 #!/usr/bin/env python3
 import argparse        # commandline arguments
 import re              # regular expression
 import dns.resolver    # to use this library, the package has to be installed  'sudo apt install python3-dnspython'
+#from datetime import date
 from subprocess import check_output  ### to use windows command https://stackoverflow.com/questions/14894993/running-windows-shell-commands-with-python
 ### Global variables
 THIS_IS_ROOT='도메인'  # domain paragraph가 시작하는 것을 알 수 있는 구문
+### https://stackoverflow.com/questions/32490629/getting-todays-date-in-yyyy-mm-dd-in-python
+#OUTPUT_FILE='output-nslookup-' + str(date.today()) + '.txt'  # 결과 파일 이름
 
 if __name__ == '__main__':
     # 이 프로그램을 실행할 때, 받아들일 arguments 2개
     parser = argparse.ArgumentParser()
-    parser.add_argument('dns_req_file', type=argparse.FileType('r', encoding='UTF-8'))
+    #parser.add_argument('dns_req_file', type=argparse.FileType('r', encoding='UTF-8'))
+    parser.add_argument('dns_req_file', type=argparse.FileType('r'))
     parser.add_argument('csr_id', type=str, nargs='?', default='000000')  ### nargs='?' make the argument optional with a default value
     args = parser.parse_args()
 
@@ -41,6 +45,7 @@ if __name__ == '__main__':
 
     domains = []
     ipMappingList = []
+    #with args.dns_req_file as requests, open(OUTPUT_FILE, 'w') as r_file:
     with args.dns_req_file as requests:
         for line in requests:
             if THIS_IS_ROOT in line: #if the line is a start of a new domain
@@ -48,10 +53,13 @@ if __name__ == '__main__':
                 ### reference: https://stackoverflow.com/questions/15340582/python-extract-pattern-matches
                 ### 2) '(?<=...)' is a positive lookbehind assertion. 이것을 사용하려면 ...에 해당하는 string이 fixed length여야 한다.
                 ### reference: https://docs.python.org/3/library/re.html
-                theRoot = (re.search(r'(?<=[:\s])[\.\-0-9a-zA-Z]+',line)).group(0)  # 도메인 뒤에 나올 수 있는 domain name 추출 (pattern: alphanumeric or - or dot)
+                theRoot = (re.search(r'(?<=[:\s])[\.0-9a-zA-Z]+',line)).group(0)  # 도메인 뒤에 나올 수 있는 domain name 추출 (pattern: alphabets or dot)
                 #print("the root: ", theRoot)
             elif re.search(r'[\d]+\.[\d]+\.[\d]+\.[\d]+', line):  ## IPv4 정보가 있는 line인지 확인한다
-                domains.append(re.search(r'(?<=[:\s])[\.\-0-9a-zA-Z]+',line).group(0) + "." + theRoot)
+                if theRoot in re.search(r'(?<=[:\s])[\.\-0-9a-zA-Z]+',line).group(0):
+                    domains.append(re.search(r'(?<=[:\s])[\.\-0-9a-zA-Z]+',line).group(0))
+                else:
+                    domains.append(re.search(r'(?<=[:\s])[\.\-0-9a-zA-Z]+',line).group(0) + "." + theRoot)
                 ipMappingList.append(re.search(r'[\d]+\.[\d]+\.[\d]+\.[\d]+', line).group(0))
 
     print("domains:   ", domains)
