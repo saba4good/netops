f776908dc7c3567f57fb46252587b9ac0ef777f0
saba4good <33079861+saba4good@users.noreply.github.com>
2022-07-10 01:36:38 +0900

    Update formatCurrentConf4AlteonL4.v.1.2.py
    /cfg/dump과 /stats/slb/dump 파일만으로 현재 SLB 설정 현황을 csv 파일 포맷으로 만든다. (마지막 virtual 설정 읽어서 처리하는 부분 업데이트 필요함)

--- formatCurrentConf4AlteonL4.v.1.2.py
+++ formatCurrentConf4AlteonL4.v.1.2.py
@@ -2,30 +2,23 @@
 v.1.2:
 - /cfg/slb/cur 대신에 /cfg/dump과 /stats/slb/dump 파일만을 사용한다.
 Objectives:
-Alteon L4 로그 파일에서 현재 세팅을 csv 파일 포맷으로 만든다.
+Alteon L4 로그 파일에서 현재 SLB 설정 현황을 csv 파일 포맷으로 만든다.
 Output format:
-Vip, Vindex, Vport, Group No, Metric, Rip, RealSvr No, Rport, Status, No. of Current Sessions
+Vip, Vport, Rip, Rports, SLB method, Virt, Group No, RealSvr No, Description, Status, No. of Current Sessions
 Input:
-3개의 파일을 읽어들인다.
+2개의 파일을 읽어들인다.
 A format: '/stats/slb/dump' 명령어의 output을 텍스트로 저장한 파일
 B format: '/cfg/dump' 명령어의 output을 텍스트로 저장한 파일
 (putty 에서 읽을 수 있는 로그 파일 저장하고, 로그 파일을 나누어서 2개의 파일로 만든다.)
 방법: 해당 파이선 프로그램과 A 파일을 모두 한 폴더 안에 넣어두고, 그 폴더에서 다음 명령으로 실행시킨다.
      python formatCurrentConf4AlteonL4.v.1.0.1.py [log file A] [log file B]
 
-- A 파일을 한 라인씩 읽으면서 dictonary 를 만든다.
+- A 파일을 한 라인씩 읽으면서 real 서버 up/down 현황 dictonary 를 만든다.
 "Real server [\d]+ stats:" ... RealSvr No 라인의 시작: RealSvr No (key pair 중 하나)
 "100.100.100.240                                0" ... No of Current Sessions (value pair 중 하나)
 "Instance Health check: " ... Rport 라인의 시작: Rport, Status (key pair, value pair 각각)
 
-- B 파일을 한 라인씩 읽으면서 output file을 만든다.
-"/c/slb/adv" ... 이전까지는 볼 필요 없음.
-"/c/slb/group" ... vport 라인의 시작: Vgroup No (key)
-"metric" ... metric 라인의 시작(없을 수 있음): metric (value)
-"[0-9]+: IP4 " ... VIP 라인의 시작: Vindex, Vip
-"action group, rport" ... vport 라인의 시작(한 vip 에 여러개 있을 수 있음): Vport, Rport, Vgroup No
-"real servers:" ... real servers 라인 전에
-"[0-9]+: [\d]+\.[\d]+\.[\d]+\.[\d]+" ... Real IP 라인의 시작: RealSvr No, Rip
+- B 파일을 한 라인씩 읽으면서 health check profile, real server profile, group profile, output file을 차례로 만든다.
 
 references:
 - https://stackoverflow.com/questions/7427101/simple-argparse-example-wanted-1-argument-3-results
@@ -40,47 +33,58 @@ import copy
 from datetime import date
 
 ##### global variables ########################################################
-## for virtual port group metric settings processing
+## for group slb method settings processing
 DEFAULT_METRIC='leastConnections'
-IS_VGRP='/c/slb/group'
-IS_METRIC='metric'
+IS_SLB_METHOD='metric'
 INIT_VALUE='-1'
 
-## for virtual port group stats processing
-#IS_RSVR='Real server [\d]+ stats:'
-#RealSvr No, Rport
-
 ## before real svr numbers in a config file
-START_OF_SETTINGS='/c/slb/adv'
-#IS_VIP='[0-9]+: IP4 '
-IS_VPORT='action group, rport'
-#IS_RIP='[0-9]+: [\d]+\.[\d]+\.[\d]+\.[\d]+'
-# Output file의 column 순서 (0부터 시작함)
-IDX_VIP=0
-IDX_VINDEX=1
-IDX_VPORT=2
-IDX_VGROUP_NO=3
-IDX_G_METRIC=4
-IDX_RIP=5
-IDX_RSVR_NO=6
-IDX_RPORT=7
-IDX_CURRSTAT=8
-IDX_CURRSESS=9
-LAST_IDX=IDX_CURRSESS ## 맨 마지막 순서인 인덱스명
+HOSTNAME_FINDER='/c/sys/ssnmp'
+FLAG_HOSTNAME=0
+START_OF_SETTINGS='/c/slb/ssl'
+IDX_HP_ID=0
+IDX_HP_IP=1
+IDX_HP_PORT=2
+IDX_HP_TYPE=3
+IDX_HP_LOGEXP=4
+HP_LAST_IDX=IDX_HP_LOGEXP
 
+IDX_RP_ID=0
+IDX_RP_IP=1
+IDX_RP_PORTS=2
+IDX_RP_DESC=3
+IDX_RP_HC=4
+RP_LAST_IDX=IDX_RP_HC
+
+IDX_GP_ID=0
+IDX_GP_SLB=1
+IDX_GP_RIDS=2
+IDX_GP_DESC=3
+IDX_GP_HC=4
+GP_LAST_IDX=IDX_GP_DESC
+
+IDX_VIP=0
+IDX_VPORT=1
+IDX_RIP=2
+IDX_RPORTS=3
+IDX_SLB_METHOD=4
+IDX_VIRT=5
+IDX_GROUP_NO=6
+IDX_RSVR_NO=7
+IDX_DESC=8
+IDX_CURRSTAT=9
+IDX_CURRSESS=10
+LAST_IDX=IDX_CURRSESS ## 맨 마지막 순서인 인덱스명
 # Tuple
 I_CURRSESS = 0
 I_CURRSTAT = 1
-
 #
 INIT_FLAG=-1
 FLAG_VIP=1
 FLAG_VPORT=2
 FLAG_RIP=3
 FLAG_MULTI_RPORT=4
-
-OUTPUT_FILE='output-l4-settings' + str(date.today()) + '.csv'  # 결과 파일 이름
-
+#OUTPUT_FILE='output-l4-settings' + str(date.today()) + '.csv'  # 결과 파일 이름
 if __name__ == '__main__':
     # 이 프로그램을 실행할 때, 받아들일 arguments 2개
     parser = argparse.ArgumentParser()
@@ -88,7 +92,6 @@ if __name__ == '__main__':
     parser.add_argument('cfg_file', type=argparse.FileType('r'), help="log for '/cfg/dump'")
 
     args = parser.parse_args()
-
     ########################################
     ######### Current Stats processing #######
     statsDic = {}
@@ -105,69 +108,110 @@ if __name__ == '__main__':
                 #print("rSvrNo, rPort: ",rSvrNo, rPort)
                 #print("currSessions, currState: ", statsDic[(rSvrNo, rPort)])
     #print ("statsDic = ", statsDic)
-    ########################################
-    ######### Config settings processing #######
-    metrDic = {}
-    vGroupNo = '0'
-    with args.cfg_file as cfg_file, \
-        open(OUTPUT_FILE, 'w') as out_file:
+    #########################################################################
+    ######### Config file processing ########################################
+    ######### health check : [profile_id, ip, ports, type, logexp] ##########
+    ######### real  : [real No., rip, rports, description] ##################
+    ######### group : [group No., slb method, [real No.s'], description ] ###
+    ######### virt  : [virt No., vip, vport, group No., description ] #######
+    with args.cfg_file as cfg_file:
         for line in cfg_file:
-            if START_OF_SETTINGS in line:   ### 프로세싱 필요한 라인을 찾는다.
+            if HOSTNAME_FINDER in line:
+                FLAG_HOSTNAME = 1
+            elif re.search(r'name', line) and FLAG_HOSTNAME:
+                hostname = (re.search(r'(?<=\").+(?=\")', line)).group(0)
+            elif START_OF_SETTINGS in line:   ### 프로세싱 필요한 라인을 찾는다.
                 break
-        prevFlag = INIT_FLAG
-        idx = 0
-        settingsTable = []
-        if re.search(r'/c/slb/real\s[\d]+', line):
-            settingsTable.append(["" for i in range(LAST_IDX+1)]) ## 새로운 데이터 row를 만든다.
-            settingsTable[-1][IDX_RSVR_NO] = (re.search(r'(?<=/c/slb/real\s)[\d]+', line)).group(0)
-            settingsTable[-1][IDX_RPORT] = ''
-        elif re.search(r'addport\s[\d]+', line):
-            settingsTable[-1][IDX_RPORT] += (re.search(r'(?<=addport\s)[\d]+', line)).group(0)
-            prevFlag = FLAG_MULTI_RPORT
-        elif re.search(r'rip\s[\d]+\.[\d]+\.[\d]+\.[\d]+', line):
-            settingsTable[-1][IDX_RIP] = (re.search(r'(?<=rip\s)[\d]+\.[\d]+\.[\d]+\.[\d]+', line)).group(0) ## "re.error: look-behind requires fixed-width patter"로 인해 ":" 앞에 오는 숫자는 하나만 보는 걸로 수정함.
-        elif re.search(r'action group, rport', line):
-            if prevFlag == FLAG_RIP:
-                settingsTable.append(copy.deepcopy(settingsTable[-1]))  ## 맨 마지막 데이터 row를 복사해서 새로운 데이터 row를 만든다.
-            settingsTable[-1][IDX_VPORT] = (re.search(r'[\w]+(?=\:\sredirect\s)', line)).group(0)
-            settingsTable[-1][IDX_VGROUP_NO] = (re.search(r'(?<=,\sgroup\s)[\d]+', line)).group(0)
-            settingsTable[-1][IDX_G_METRIC] = metrDic[settingsTable[-1][IDX_VGROUP_NO]]
-            settingsTable[-1][IDX_RPORT] = (re.search(r'(?<=,\srport\s)[\w]+', line)).group(0)
-            prevFlag = FLAG_VPORT
-        elif re.search(r'vip\s[\d]+\.[\d]+\.[\d]+\.[\d]+', line):
-            settingsTable[-1][IDX_VIP] = (re.search(r'(?<=vip\s)[\d]+\.[\d]+\.[\d]+\.[\d]+', line)).group(0)
-            settingsTable[-1][IDX_VINDEX] = (re.search(r'[\d]+(?=\:\sIP4\s)', line)).group(0)  ## 맨 마지막에 만들어진 데이터 row를 변경함.
-            #print("settingsTable: ", settingsTable[idx] )
-            settingsTable[-1][IDX_CURRSTAT] = statsDic[(settingsTable[-1][IDX_RSVR_NO], settingsTable[-1][IDX_RPORT])][I_CURRSTAT]
-            settingsTable[-1][IDX_CURRSESS] = statsDic[(settingsTable[-1][IDX_RSVR_NO], settingsTable[-1][IDX_RPORT])][I_CURRSESS]
-            idx += 1
-            prevFlag = FLAG_RIP
-
-    out_file.write("Vip, Vindex, Vport, Group No, Metric, Rip, RealSvr No, Rport, Status, No. of Current Sessions\n")
-    for row in settingsTable:
-        for j, value in enumerate(row):
-            out_file.write(value)
-            if j != LAST_IDX:
-                out_file.write(", ")
-            else:
-                out_file.write("\n")
-
-
-        #### metric settings processing ###########
+        ######### health check : [profile_id, ip, ports, type, logexp] ##########
+        healthProfiles = []
         for line in cfg_file:
-            if IS_VGRP in line:
-                try:
-                    if metrDic[vGroupNo] == INIT_VALUE:  ### test if the privous virtual group is all set. (Alteon은 metric이 default인 경우, 해당 라인이 config에 없다. 따라서 다음 vgroup 에서 확인해줘야함.)
-                        metrDic[vGroupNo] = DEFAULT_METRIC
-                except KeyError:  ### when the key isn't in the dictonary
-                    pass
-                vGroupNo = (re.search(r'(?<=/c/slb/group\s)[\d]+', line)).group(0) ## extract the virtual group number from the line.
-                metrDic[vGroupNo] = INIT_VALUE
-            elif IS_METRIC in line:
-                metrDic[vGroupNo] = (re.search(r'(?<=metric\s)[\w]+', line)).group(0)
-        try:
-            if metrDic[vGroupNo] == INIT_VALUE:
-                metrDic[vGroupNo] = DEFAULT_METRIC
-        except ValueError:
-            pass
-    #print ("metrDic = ", metrDic)
+            if re.search(r'/c/slb/advhc/health\s[\w_]+', line):
+                healthProfiles.append(["" for i in range(HP_LAST_IDX+1)])
+                healthProfiles[-1][IDX_HP_ID] = (re.search(r'(?<=/c/slb/advhc/health\s)[\w_]+', line)).group(0)
+                healthProfiles[-1][IDX_HP_TYPE] = (re.search(r'[\w]+$', line)).group(0)
+            elif re.search(r'dport\s[\d]+', line):
+                healthProfiles[-1][IDX_HP_PORT] = (re.search(r'(?<=dport\s)[\d]+', line)).group(0)
+            elif re.search(r'dest\s[\d]+', line):
+                healthProfiles[-1][IDX_HP_IP] = (re.search(r'[\d]+\.[\d]+\.[\d]+\.[\d]+', line)).group(0)
+            elif re.search(r'logexp\s\"', line):
+                healthProfiles[-1][IDX_HP_LOGEXP] = (re.search(r'(?<=\").+(?=\")', line)).group(0)
+            elif re.search(r'/c/slb$', line): ## health check 구문을 빠져나가면
+                break                         ## for loop을 중단하여 cfg_file을 그만 읽는다.
+        ######### real  : [real No., rip, rports, description] ##################
+        realProfiles = []
+        groupProfiles = []
+        for line in cfg_file:
+            if re.search(r'/c/slb/real\s[\d]+', line):
+                realProfiles.append(["" for i in range(RP_LAST_IDX+1)])
+                realProfiles[-1][IDX_RP_ID] = (re.search(r'(?<=/c/slb/real\s)[\d]+', line)).group(0)
+            elif re.search(r'rip\s[\d]+\.[\d]+\.[\d]+\.[\d]+', line):
+                realProfiles[-1][IDX_RP_IP] = (re.search(r'(?<=rip\s)[\d]+\.[\d]+\.[\d]+\.[\d]+', line)).group(0)
+            elif re.search(r'name\s[\d]+', line):
+                realProfiles[-1][IDX_RP_DESC] = (re.search(r'(?<=\").+(?=\")', line)).group(0)
+            elif re.search(r'health\s[\w_]+', line):
+                realProfiles[-1][IDX_RP_HC] = (re.search(r'(?<=health\s)[\w_]+', line)).group(0)
+            elif re.search(r'addport\s[\d]+', line):
+                if not realProfiles[-1][IDX_RP_PORTS]:      ## real ports에 이미 값이 있으면
+                    realProfiles[-1][IDX_RP_PORTS] += ','   ## 콤마를 붙여줘라
+                realProfiles[-1][IDX_RP_PORTS] = (re.search(r'(?<=\s)[\d]+', line)).group(0)
+            elif re.search(r'/c/slb/group\s[\d]+', line):
+                groupProfiles.append(["" for i in range(GP_LAST_IDX+1)])
+                groupProfiles[-1][IDX_GP_ID] = (re.search(r'(?<=/c/slb/group\s)[\d]+', line)).group(0)
+                break
+        ######### group : [group No., slb method, [real No.s'], description ] ###
+        settingsTable = []
+        for line in cfg_file:
+            if re.search(r'/c/slb/group\s[\d]+', line):
+                groupProfiles.append(["" for i in range(GP_LAST_IDX+1)])
+                groupProfiles[-1][IDX_GP_ID] = (re.search(r'(?<=/c/slb/group\s)[\d]+', line)).group(0)
+            elif re.search(r'add\s[\d]+', line):
+                if groupProfiles[-1][IDX_GP_RIDS]:      ## real rid에 값이 없으면
+                    groupProfiles[-1][IDX_GP_RIDS] = []
+                groupProfiles[-1][IDX_GP_RIDS].append[(re.search(r'(?<=\s)[\d]+', line)).group(0)]
+            elif re.search(r'metric\s[\w]+', line):
+                groupProfiles[-1][IDX_GP_SLB] = (re.search(r'(?<=\s)[\w]+', line)).group(0)
+            elif re.search(r'name\s[\d]+', line):
+                groupProfiles[-1][IDX_GP_DESC] = (re.search(r'(?<=\").+(?=\")', line)).group(0)
+            elif re.search(r'health\s[\w]+', line):
+                groupProfiles[-1][IDX_GP_HC] = (re.search(r'(?<=\s)[\w]+', line)).group(0)
+            elif re.search(r'/c/slb/virt\s[\d]+', line):
+                settingsTable.append(["" for i in range(LAST_IDX+1)])
+                settingsTable[-1][IDX_VIRT] = (re.search(r'(?<=/c/slb/virt\s)[\d]+', line)).group(0)
+                break
+        ######### virt  : [virt No., vip, vport, group No., description ] #######
+        #idx = 0
+        #prevFlag = INIT_FLAG
+        vip = ''
+        for line in cfg_file:
+            if re.search(r'/c/slb/virt\s[\d]+', line):
+                settingsTable.append(["" for i in range(LAST_IDX+1)])
+                settingsTable[-1][IDX_VIRT] = (re.search(r'(?<=/c/slb/virt\s)[\d]+', line)).group(0)
+                settingsTable[-1][IDX_VPORT] = (re.search(r'(?<=service\s)[\d]+', line)).group(0)
+            elif re.search(r'group\s[\d]+', line):
+                settingsTable[-1][IDX_GROUP_NO] = (re.search(r'(?<=group\s)[\d]+', line)).group(0)
+                # groupProfiles 테이블에서 settingsTable 레코드 채워야 함.
+                # realProfiles 테이블에서도 내용 가져와야 함.
+                settingsTable[-1][IDX_CURRSTAT] = statsDic[(settingsTable[-1][IDX_RSVR_NO], settingsTable[-1][IDX_RPORT])][I_CURRSTAT]
+                settingsTable[-1][IDX_CURRSESS] = statsDic[(settingsTable[-1][IDX_RSVR_NO], settingsTable[-1][IDX_RPORT])][I_CURRSESS]
+            elif re.search(r'rport\s[\d]+', line):
+                settingsTable[-1][IDX_RPORT] = (re.search(r'(?<=rport\s)[\d]+', line)).group(0)
+                #if not settingsTable[-1][IDX_RPORT]:   ## rport 0, 즉 multi port인 경우
+                    ################ ID로 테이블에서 레코드를 찾아야 하지만, IP와 같은 레코드 일부를 이용하여 ID를 찾는 경우가 없다면, dictonary가 훨씬 간편할 것이다.
+                    # settingsTable[-1][IDX_RPORT] = groupProfiles[-1][IDX_GP_ID] settingsTable[-1][IDX_GROUP_NO]
+            elif re.search(r'vip\s[\d]+\.[\d]+\.[\d]+\.[\d]+', line):
+                settingsTable[-1][IDX_VIP] = (re.search(r'(?<=vip\s)[\d]+\.[\d]+\.[\d]+\.[\d]+', line)).group(0)
+                vip = settingsTable[-1][IDX_VIP]
+            elif re.search(r'vname\s[\d]+', line):
+                settingsTable[-1][IDX_DESC] = (re.search(r'(?<=\").+(?=\")', line)).group(0)
+            #print("settingsTable: ", settingsTable[idx] )
+
+    output_file=hostname + '-cfg-' + str(date.today()) + '.csv'  # 결과 파일 이름
+    with open(output_file, 'w') as out_file:
+        out_file.write("Vip, Vport, Rip, Rports, SLB method, Virt, Group No, RealSvr No, Description, Status, No. of Current Sessions\n")
+        for row in settingsTable:
+            for j, value in enumerate(row):
+                out_file.write(value)
+                if j != LAST_IDX:
+                    out_file.write(", ")
+                else:
+                    out_file.write("\n")
